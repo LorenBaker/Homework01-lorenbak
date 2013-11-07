e678952e12943a4b68545698b89daf49eb6d30ed
LorenBaker <LorenABaker@comcast.net>
2013-11-06 16:02:35 -0800

    End of Day 11-6-2013

--- gen/com/lbconsulting/CP210/homework01_lorenbak/R.java
+++ gen/com/lbconsulting/CP210/homework01_lorenbak/R.java
@@ -38,13 +38,17 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int Landscape270Degrees=0x7f05000a;
-        public static final int Landscape90Degrees=0x7f050008;
+        public static final int Landscape180Degrees=0x7f05000c;
+        public static final int Landscape270Degrees=0x7f05000e;
+        public static final int Landscape90Degrees=0x7f050009;
+        public static final int NaturalLandscape=0x7f050008;
         public static final int NaturalPortrait=0x7f050007;
-        public static final int NoActivitySettings=0x7f05000c;
+        public static final int NoActivitySettings=0x7f050010;
         public static final int OK=0x7f050005;
-        public static final int OrientationNotFound=0x7f05000b;
-        public static final int Portrait180Degrees=0x7f050009;
+        public static final int OrientationNotFound=0x7f05000f;
+        public static final int Portrait180Degrees=0x7f05000b;
+        public static final int Portrait270Degrees=0x7f05000d;
+        public static final int Portrait90Degrees=0x7f05000a;
         public static final int action_orientation=0x7f050006;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
